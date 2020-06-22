c1bec6d95d605013440b24d54d77aea0542fcefd
MFRamon <ramon.manfig@gmail.com>
2020-06-22 17:32:31 -0500

    Changes

--- DockerNodeAPI/environments/env.production.ts
+++ DockerNodeAPI/environments/env.production.ts
@@ -6,7 +6,7 @@ export default {
     },
     MONGODB: {
         PORT: 27017,
-        HOST: '192.168.118.128',
+        HOST: '127.0.0.1',
         USER_NAME: 'dba-ramon',
         USER_PASSWORD: 'mongoPass123',
         DEFAULT_DATABASE: 'dbAuth'
